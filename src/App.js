030548d7f58b8cb2729d1d512dcd6f6983baeb34
Anouar-Aouini <anoirawini1571@gmail.com>
2021-04-25 01:57:02 +0100

    React Hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './Components/NavBar/NavBar';
+import {Data} from './Components/Data/Data';
+import MovieList from "./Components/MovieList/MovieList"
+import React,{useState} from 'react';
 
 function App() {
+  const [data,setData]=useState(Data)
+  const [movieSearch,setMovieSearch]=useState("");
+  const [movieSearchRating,setMovieSearchRating]=useState(0);
+  const handleClick=(newMovie)=>{
+    setData([...data,newMovie])
+  }
+  console.log(movieSearchRating)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar handleClick={handleClick} setMovieSearch={setMovieSearch} setMovieSearchRating={setMovieSearchRating}/>
+      <MovieList Data={data} movieSearch={movieSearch} movieSearchRating={movieSearchRating}></MovieList>
+      
     </div>
-  );
-}
-
+  );}
 export default App;
